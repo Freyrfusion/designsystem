7f041c955f326081d351fd7403cc0299ac56bcde
vchelmeda <vchelmeda@VINODC2>
2022-03-15 18:04:50 +0530

    changes

--- UI/src/app/app.component.ts
+++ UI/src/app/app.component.ts
@@ -18,9 +18,38 @@ export class AppComponent {
 
   }
   
-  SearchFunction() {
-  }
+ 
+  
+SearchFunction() {
 
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("searchInput");
+ 
+  filter = input.value.toUpperCase();
+
+  let menu = document.getElementById('dynamic-tabs');
+ // let children = menu.children;
+ // console.log(children);
+  
+  table =  document.querySelectorAll(".dynamic-tabs");
+ // table = document.getElementsByClassName("dynamic-tabs");
+
+  //tr = table.getElementsByTagName("li");
+ tr = menu.children;
+
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("a")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+        
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+}
   hasRoute(route: string) {
     return this.router.url.includes(route);
   }
