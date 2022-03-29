f3fe7fae6db13193d9e551182604e30399b326fe
aarumrugam <aarumrugam@ASHWINA2>
2022-03-30 02:13:21 +0530

    modal popup

--- UI/src/app/components/modals/modals.component.ts
+++ UI/src/app/components/modals/modals.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as $ from 'jquery';
 @Component({
   selector: 'app-modals',
   templateUrl: './modals.component.html',
@@ -10,6 +10,47 @@ export class ModalsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    $(".css-pop#pop1").click(function () {    
+      $(".css-pop-overlay#pop1").addClass("active");      
+    });
+    $(".css-pop#pop2").click(function () {    
+      $(".css-pop-overlay#pop2").addClass("active");      
+    });
+    $(".css-pop#pop3").click(function () {    
+      $(".css-pop-overlay#pop3").addClass("active");      
+    });
+    $(".css-pop#pop4").click(function () {    
+      $(".css-pop-overlay#pop4").addClass("active");      
+    });
+    $(".css-pop#pop5").click(function () {    
+      $(".css-pop-overlay#pop5").addClass("active");      
+    });
+    $(".css-pop#pop6").click(function () {    
+      $(".css-pop-overlay#pop6").addClass("active");      
+    });
+    $(".css-pop#pop7").click(function () {    
+      $(".css-pop-overlay#pop7").addClass("active");      
+    });
+    $(".css-pop#pop8").click(function () {    
+      $(".css-pop-overlay#pop8").addClass("active");      
+    });
+  
+  
+  
+    $(".css-pop.close").click(function () {    
+      $(".css-pop-overlay").removeClass("active");      
+    });
+  
+
   }
 
+  
+
 }
+
+
+
+	
+
+
