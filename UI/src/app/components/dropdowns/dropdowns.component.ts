3a886e4ff1085d53fc8ce8738e0be50fc1dcdb96
vchelmeda <vchelmeda@VINODC2>
2022-07-07 12:50:51 +0530

    Design changes

--- UI/src/app/components/dropdowns/dropdowns.component.ts
+++ UI/src/app/components/dropdowns/dropdowns.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-dropdowns',
@@ -10,6 +11,23 @@ export class DropdownsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+   
+    var menuHeader = $('.dropdown-header');
+   
+    // var menuHeadertoggle = $('.dropdown-header').attr('data-toggle');
+    // var menuContainer = $('.dropdown-header ~ .dropdown-container').attr('id'); //$("#"+ menuHeadertoggle).attr('id');
+        menuHeader.click(function(){
+          var select = $(this);
+          var menuHeadertoggle = select.attr('data-toggle');
+          //var menuContainer = $('.dropdown-header ~ .dropdown-container').attr('id');
+          var menuContainer = select.siblings(".dropdown-container").attr('id');
+       
+          $("#"+ menuContainer).toggleClass("active");
+        });
   }
+
+ 
+  
+
 
 }
