da88045574476f3a74ba92b1b4dfd4949fd753e5
Bhaskar Potta <bhaskar.potta@FreyrSolutions.com>
2022-07-19 15:12:00 +0530

    Angular tabs

--- UI/src/app/components/headers/headers.component.ts
+++ UI/src/app/components/headers/headers.component.ts
@@ -1,17 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import * as $ from 'jquery';
+import { CommonService } from 'src/app/services/common.service';
+import { CopyserviceService } from 'src/app/services/copy/copyservice.service';
 @Component({
   selector: 'app-headers',
   templateUrl: './headers.component.html',
   styleUrls: ['./headers.component.scss']
 })
 export class HeadersComponent implements OnInit {
-
-  constructor() {
+  commontablist:any;
+  selectedList:any;
+  copyToClipboard:any;
+  copytxt: any;
+  i:any;
+  constructor(public _common:CommonService, public _copy:CopyserviceService) {
 
   }
 
   ngOnInit(): void {
+    this.commontablist=this._common.ngtabList();
+    this.selectedList = 0;
+    this.copyToClipboard=this._copy.copyToClipboard;
+    this.copytxt = this._copy.copytxt;
+    
     $(document).ready(function(){
       $('.header-profile-section').click(function() {
         $('.profile-dropdown-content').slideToggle('slow');
@@ -39,6 +50,10 @@ export class HeadersComponent implements OnInit {
       }            
   });
     
+  
+  }
+  openTabList(commontablist:any){
+    this.selectedList = commontablist;
 
   }
   
