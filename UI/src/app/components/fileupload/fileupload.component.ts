66182a0d434aee0bfc3a3ccfe53ecb15e45c8100
Mahalakshmi Vadra <Mahalakshmi.Vadra@FreyrSolutions.com>
2022-08-05 12:44:18 +0530

    UI Changes

--- UI/src/app/components/fileupload/fileupload.component.ts
+++ UI/src/app/components/fileupload/fileupload.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+import { CommonService } from 'src/app/services/common.service';
+import { CopyserviceService } from 'src/app/services/copy/copyservice.service';
 
 @Component({
   selector: 'app-fileupload',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fileupload.component.scss']
 })
 export class FileuploadComponent implements OnInit {
-
-  constructor() { }
+  selectedList: any;
+  commontablist: any;
+  copyToClipboard: any;
+  copytxt: any;
+  constructor(public _common: CommonService, public _copy: CopyserviceService) {
+    
+   }
 
   ngOnInit(): void {
+    this.commontablist = this._common.ngtabList();
+    this.selectedList = 0;
+    this.copyToClipboard = this._copy.copyToClipboard;
+    this.copytxt = this._copy.copytxt;
+  }
+  openTabList(commontablist: any) {
+    this.selectedList = commontablist;
   }
 
 }
