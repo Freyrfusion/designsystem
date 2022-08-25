b085dc41e7fe40653348ccbaf04ef80d6e40166f
Bhaskar Potta <bhaskar.potta@FreyrSolutions.com>
2022-08-25 13:13:46 +0530

    Search box

--- UI/src/app/components/searchbox/searchbox.component.ts
+++ UI/src/app/components/searchbox/searchbox.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonService } from 'src/app/services/common.service';
 import { CopyserviceService } from 'src/app/services/copy/copyservice.service';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-searchbox',
@@ -19,6 +20,14 @@ export class SearchboxComponent implements OnInit {
     this.selectedList = 0;
     this.copyToClipboard = this._copy.copyToClipboard;
     this.copytxt = this._copy.copytxt;
+    $("#inpt_search").on('focus', function () {
+      $(this).parent('label').addClass('active');
+    });
+    
+    $("#inpt_search").on('blur', function () {
+      if($(this).val().length == 0)
+        $(this).parent('label').removeClass('active');
+    });
   }
   openTabList(commontablist: any) {
     this.selectedList = commontablist;
