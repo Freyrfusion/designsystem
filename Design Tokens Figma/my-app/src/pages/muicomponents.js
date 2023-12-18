58d25743bc29f154f0e56838a381464411433c8c
vinodchelmeda <vinodchelmeda@gmail.com>
2023-12-18 13:12:36 +0530

    Design Routing changes

--- Design Tokens Figma/my-app/src/pages/muicomponents.js
+++ Design Tokens Figma/my-app/src/pages/muicomponents.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import Sidebar from '../components/sidebar.js';
+import React, { useState } from 'react';
+
+// Components
+import Muitable from '../components/muitable.js';
+import Muibuttons from '../components/muibuttons.js';
+import Muicollapse from '../components/muicollapse.js';
+
+import '../assets/scss/sitemain.scss';
 
 const MuiComponents = () => {
 
-      return (
-        <div>
-            <Sidebar />
-            <center>
-                <br/><br/>
-          <h1>We are preparing MuiComponents</h1>
-          </center>
-        </div>
-      );
+  const [currentPage, setCurrentPage] = useState('home');
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'muitable':
+        return <Muitable />;
+      case 'muibuttons':
+        return <Muibuttons />;
+      case 'muicollapse':
+        return <Muicollapse />;
+      default:
+        return <Muitable />;
+    }
+  };
+
+  return (
+    <div>
+
+<div class="sidebar" >
+
+  
+<h5>MUI Components</h5>
+<ul id="dynamic-tabs">
+    <li className={currentPage==="muitable" && 'activetab'} onClick={() => setCurrentPage('muitable')}>MUI Table</li>
+    <li className={currentPage==="muibuttons" && 'activetab'} onClick={() => setCurrentPage('muibuttons')}>MUI Buttons</li>
+    <li className={currentPage==="muicollapse" && 'activetab'} onClick={() => setCurrentPage('muicollapse')}>MUI Collapse</li>
+</ul>
+
+
+</div>
+
+
+      {renderPage()}
+    </div>
+  );
 };
 
 export default MuiComponents;
