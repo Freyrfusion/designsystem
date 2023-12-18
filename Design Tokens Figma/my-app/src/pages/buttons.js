4cbfa6edf6e67e0c53416ddae2ca64e96f2d62ec
vishnu <vishnu.kondapalli@freyrsolutions.com>
2023-12-18 16:49:10 +0530

    Icon buttons

--- Design Tokens Figma/my-app/src/pages/buttons.js
+++ Design Tokens Figma/my-app/src/pages/buttons.js
@@ -5,16 +5,72 @@ const Buttons = () => {
         <div>
             <center>
             <h1>We are preparing Buttons</h1>
+
+            <h3> Buttons</h3>
+            <p> The Button component has an appearance with different propertys and values</p>
             <button type="button" class="btn btn-primary"> Primary </button>
             <button type="button" class="btn btn-success"> Success </button>
             <button type="button" class="btn btn-danger"> Danger </button>
             <button type="button" class="btn btn-warning"> Warning </button>
             <button type="button" class="btn btn-secondary">Secondary</button>
+            <a href="#" class="btn btn-primary-link"> Link </a>
+       </center>
+       <hr></hr>
+
+          <center>
+            <h3> Left Icon Buttons</h3>
+            <p> The Button component has an appearance with different propertys and values</p>
+            <button type="button" class="btn btn-primary"><i class="icon-hash2"></i> Primary </button>
+            <button type="button" class="btn btn-success"><i class="icon-hash2"></i> Success </button>
+            <button type="button" class="btn btn-danger"> <i class="icon-hash2"></i>Danger </button>
+            <button type="button" class="btn btn-warning"><i class="icon-hash2"></i> Warning </button>
+            <button type="button" class="btn btn-secondary"><i class="icon-hash2"></i>Secondary</button>
+            <a href="#" class="btn btn-primary-link"> <i class="icon-hash2"></i>Link </a>
+          </center>
+          <hr></hr>
+
+          <center>
+            <h3>Right Icon Buttons</h3>
+            <p> The Button component has an appearance with different propertys and values</p>
+            <button type="button" class="btn btn-primary"> Primary <i class="icon-hash2"></i> </button>
+            <button type="button" class="btn btn-success"> Success <i class="icon-hash2"></i></button>
+            <button type="button" class="btn btn-danger"> Danger <i class="icon-hash2"></i></button>
+            <button type="button" class="btn btn-warning"> Warning <i class="icon-hash2"></i></button>
+            <button type="button" class="btn btn-secondary">Secondary <i class="icon-hash2"></i></button>
+            <a href="#" class="btn btn-primary-link"> Link <i class="icon-hash2"></i> </a>
+          </center>
+          <hr></hr>
           
+          <center>
+            <h3>Left Right Icons</h3>
+            <p> The Button component has an appearance with different propertys and values</p>
+            <button type="button" class="btn btn-primary"><i class="icon-menu3"></i> Primary <i class="icon-down-open"></i></button>
+            <button type="button" class="btn btn-success"><i class="icon-menu3"></i> Success <i class="icon-down-open"></i></button>
+            <button type="button" class="btn btn-danger"><i class="icon-menu3"></i> Danger <i class="icon-down-open"></i></button>
+            <button type="button" class="btn btn-warning"><i class="icon-menu3"></i> Warning  <i class="icon-down-open"></i></button>
+            <button type="button" class="btn btn-secondary"><i class="icon-menu3"></i> Secondary  <i class="icon-down-open"></i></button>
+            <a href="#" class="btn btn-primary-link"><i class="icon-hash2"></i>  Link <i class="icon-down-open"></i></a>
+          </center>
+          <hr></hr>
+
+
+          <center>
+            <h3>Icon Buttons</h3>
+            <p> The Button component has an appearance with different propertys and values</p>
+            <button type="button" class="btn btn-primary"> <i class="icon-thumbnail"></i> <i class="icon-down-open"></i></button>
+            <button type="button" class="btn btn-success"> <i class="icon-thumbnail"></i> <i class="icon-down-open"></i></button>
+            <button type="button" class="btn btn-danger"> <i class="icon-thumbnail"></i> <i class="icon-down-open"></i></button>
+            <button type="button" class="btn btn-warning"> <i class="icon-thumbnail"></i> <i class="icon-down-open"></i></button>
+            <button type="button" class="btn btn-secondary"> <i class="icon-thumbnail"></i> <i class="icon-down-open"></i></button>
+
            
-     
+
            
           </center>
+          <hr></hr>
+
+
+
         </div>
 
         
